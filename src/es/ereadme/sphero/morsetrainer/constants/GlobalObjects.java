6ba2d3396dfe053f66ca7a3f33d205639523c9f5
Bernardo Botella Corbí <estemaci@gmail.com>
2014-01-03 18:11:05 +0100

    I forgot to add some files.

---
+++ src/es/ereadme/sphero/morsetrainer/constants/GlobalObjects.java
@@ -0,0 +1,9 @@
+package es.ereadme.sphero.morsetrainer.constants;
+
+import orbotix.sphero.Sphero;
+import orbotix.view.connection.SpheroConnectionView;
+
+public class GlobalObjects {
+	public static Sphero mRobot = null;
+	public static SpheroConnectionView mSpheroConnectionView = null;
+}
