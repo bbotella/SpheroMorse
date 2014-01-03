f8dd1e4107ba182c5a24148da71dd11042544c0b
Bernardo Botella Corbí <estemaci@gmail.com>
2014-01-03 17:54:07 +0100

    App already connects to sphero.

--- src/es/ereadme/sphero/morsetrainer/MainActivity.java
+++ src/es/ereadme/sphero/morsetrainer/MainActivity.java
@@ -1,15 +1,45 @@
 package es.ereadme.sphero.morsetrainer;
 
-import android.os.Bundle;
+import orbotix.robot.base.Robot;
+import orbotix.sphero.ConnectionListener;
+import orbotix.sphero.Sphero;
+import orbotix.view.connection.SpheroConnectionView;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private Sphero mRobot;
+
+    /** The Sphero Connection View */
+    private SpheroConnectionView mSpheroConnectionView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mSpheroConnectionView = (SpheroConnectionView) findViewById(R.id.sphero_connection_view);
+        mSpheroConnectionView.addConnectionListener(new ConnectionListener() {
+
+            @Override
+            public void onConnected(Robot robot) {
+                mRobot = (Sphero) robot;
+                //mRobot.getCollisionControl().addCollisionListener(mCollisionListener);
+                mRobot.getCollisionControl().startDetection(45, 45, 100, 100, 100);
+            }
+
+            @Override
+            public void onConnectionFailed(Robot sphero) {
+                // let the SpheroConnectionView handle or hide it and do something here...
+            }
+
+            @Override
+            public void onDisconnected(Robot sphero) {
+                mSpheroConnectionView.startDiscovery();
+            }
+        });
 	}
 
 	@Override
